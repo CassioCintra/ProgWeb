b9771a8c2dfe7bc4c4d8ad86ea85ac20fa9c7ed9
CassioCintra <cassiocr2012@hotmail.com>
2024-04-04 10:25:15 -0300

    React-hooks 1.2 "Encerrando o React-hooks"

--- react-hooks/src/exercicios/06.jsx
+++ react-hooks/src/exercicios/06.jsx
@@ -3,30 +3,79 @@ import * as React from 'react'
 import { fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
-   
+  //const [pokemon, setPokemon] = React.useState(null)
+  //const [error, setError] = React.useState(null)
+  //const [status, setStatus] = React.useState('idle')
+  
+  const [state, setState] = React.useState({
+    pokemon: null,
+    error: null,
+    status: 'idle'
+  })
+  const {pokemon, error, status} = state
+
   React.useEffect(() => {
     if(!pokemonName) return
     
-    setPokemon(null)
-    setError(null)
+    //setPokemon(null)
+    //setError(null)
+    //setStatus('pending')
+    setState = ({
+      pokemon:null,
+      error:null,
+      status:'pending'})
 
     fetchPokemon(pokemonName).then( //Sucesso
-      pokemonData => setPokemon(pokemonData))
+      pokemonData => {
+        //setPokemon(pokemonData),
+        //setStatus('resolved')
+        setState({
+          ...state,
+          pokemon: pokemonData,
+          status: 'resolved'
+        })
+      }
+      )
     .catch( //Falha
-      error => setError(error))
-    
+      error => {
+        //setError(error),
+        //setStatus('rejected')
+        setState({
+          ...state,
+          error,
+          status: 'rejected'
+        })
+      }
+    )
   }, [pokemonName])
 
-  if(error) return (
-    <div role='alert'>
-      <pre stryle= {{whiteSpace:'normal'}}>{error.message}</pre>
-    </div>
-  )
-  if(! pokemonName) return 'Informe um Pokémon'
-  if(pokemonName && !pokemon) return <PokemonInfoFallback name={pokemonName}/>
-  if(pokemon) return <PokemonDataView pokemon={pokemon}/>
+  React.useEffect(() => {
+    console.count('Componente atualizou.')
+  })
+
+//if(error) return (
+//  <div role='alert'>
+//    <pre stryle= {{whiteSpace:'normal'}}>{error.message}</pre>
+//  </div>
+//)
+//if(! pokemonName) return 'Informe um Pokémon'
+//if(pokemonName && !pokemon) return <PokemonInfoFallback name={pokemonName}/>
+//if(pokemon) return <PokemonDataView pokemon={pokemon}/>
+
+  switch(status){
+    case 'idle':
+      return 'Informe o Pokemon!'
+    case 'pending':
+      return <PokemonInfoFallback name={pokemonName} />
+    case 'resolved':
+      return <PokemonDataView pokemon={pokemon} />
+    default: // rejected
+      return(
+        <div role= "alert">
+          <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+      )
+  }
 }
 
 function Exercicio06() {
